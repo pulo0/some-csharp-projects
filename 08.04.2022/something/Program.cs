d404d2d29112cddf29b9541c78dc7f4b20f0a802
pulo0 <82182605+pulo0@users.noreply.github.com>
2022-04-09 11:28:30 +0200

    changed some code
    yeah yeah, it's this again. don't mind it

---
+++ 08.04.2022/something/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace something
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Human> dictionary = new Dictionary<int, Human>();
+
+            Human human = new Human();
+            Human human2 = new Human();
+
+            human.hairColor = "Brown";
+            human.age = 1;
+            human.eye = "Blue";
+
+            human2.hairColor = "Blue";
+            human2.age = 2;
+            human2.eye = "Yellow";
+
+            Console.WriteLine("Human1");
+            Console.WriteLine(human.hairColor);
+            Console.WriteLine(human.eye);
+            Console.WriteLine(human.age);
+
+            Console.WriteLine("Human2");
+            Console.WriteLine(human2.hairColor);
+            Console.WriteLine(human2.eye);
+            Console.WriteLine(human2.age);
+        }
+    }
+
+    class Human
+    {
+        public string hairColor;
+        public int age;
+        public string eye;
+
+    }
+}
